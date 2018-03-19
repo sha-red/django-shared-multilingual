9995394b53d97cc1f039cdb4cf0f0c0eecf103b4
Erik Stein <erik@classlibrary.net>
2018-03-19 12:58:31 +0100

    Added admin language switcher (j@thing.net).

---
+++ shared/multilingual/static/multilingual/js/switch_language.js
@@ -0,0 +1,46 @@
+// TODO Add lang attribute to form fields, use lang attribute to query fields
+function showLanguages(languages) {
+    document.querySelectorAll('.form-row, .field-box').forEach(function(row) {
+        var lang = [];
+        row.classList.forEach(function(cls) {
+            if (cls) {
+                var l = cls.split('_').pop();
+                if (l.length == 2 || l.length == 3) {
+                    lang.push(l)
+                }
+            }
+        });
+        if (lang.length) {
+            var display = 'none';
+            if (lang.filter(function(l) { return languages.includes(l); }).length > 0) {
+                display = 'block'
+            }
+            row.style.display = display;
+        }
+    })
+    localStorage.admin_languages = languages.join(' ')
+    document.querySelectorAll('.language-select').forEach(function(a) {
+        if (a.dataset.languages == localStorage.admin_languages) {
+            a.classList.add('selected')
+        } else {
+            a.classList.remove('selected')
+        }
+    })
+}
+
+(function($) {
+    $(document).ready(function() {
+        $('.language-select').on({
+            click: function() {
+                showLanguages(this.dataset.languages.split(' '))
+            }
+        })
+        var languages = $('.language-select.selected').data('languages')
+        if (localStorage.admin_languages) {
+            languages = localStorage.admin_languages
+        }
+        if (languages) {
+            showLanguages(languages.split(' '))
+        }
+    })
+})(django.jQuery);
