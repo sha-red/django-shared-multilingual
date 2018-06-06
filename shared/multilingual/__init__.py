d56c1f5bb813d53a9c3c9b2d4bdb38c02e848d08
j <j@mailb.org>
2018-06-06 14:22:48 +0200

    include git commit count in version and update in setup.py

--- shared/multilingual/__init__.py
+++ shared/multilingual/__init__.py
@@ -1,4 +1,11 @@
 from __future__ import unicode_literals
 
 __version__ = '0.1.1'
-VERSION = tuple(int(d) for d in __version__.split('.'))
+
+try:
+    from ._version import __version__
+except ImportError:
+    pass
+
+VERSION = __version__.split('+')
+VERSION = tuple(list(map(int, VERSION[0].split('.'))) + VERSION[1:])
